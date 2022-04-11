7f3a97d11c3c92e2f0a2a28b04402725b0c30489
devesteves <estevesrodri@outlook.com>
2022-04-10 21:03:43 -0300

    Update line size

--- eur_btc_rates.py
+++ eur_btc_rates.py
@@ -11,10 +11,17 @@ driver.get("https://finance.yahoo.com/quote/BTC-EUR/history")
 
 with open('eur_btc_rates.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=';', quotechar='|')
-    spamwriter.writerow([driver.find_element(By.CSS_SELECTOR, "thead > tr th:nth-child(1) > span").text, # writes "Date" title element
-    driver.find_element(By.CSS_SELECTOR, "thead > tr th:nth-child(5) > span").text]) # writes "Close" title element
+    spamwriter.writerow([driver.find_element(By.CSS_SELECTOR,
+     "thead > tr th:nth-child(1) > span").text, # writes "Date" title element
+    driver.find_element(By.CSS_SELECTOR, "thead > tr th:nth-child(5) > span")
+    .text]) # writes "Close" title element
     for i in range(1,11):                   #iterates "date" and "close" values until 10 elements
         stringIterator = str(i)             # turns int iterator into string
-        spamwriter.writerow([driver.find_element(By.CSS_SELECTOR,"tbody > tr:nth-child("+stringIterator+") > td.Py\(10px\).Ta\(start\).Pend\(10px\) > span").text, # writes the respective Date values 
-        driver.find_element(By.CSS_SELECTOR, "tbody > tr:nth-child("+stringIterator+") > td:nth-child(5) > span").text]) # writes close values for the respective Date
+        spanElementCloseValue = "5"         # Location of span in element
+        spamwriter.writerow([driver.find_element(By.CSS_SELECTOR,
+        "tbody > tr:nth-child("+stringIterator+") > td.Py\(10px\).Ta\(start\).Pend\(10px\) > span")
+        .text, # writes the respective Date values 
+        driver.find_element(By.CSS_SELECTOR, 
+        "tbody > tr:nth-child("+stringIterator+") > td:nth-child("+spanElementCloseValue+") > span")
+        .text]) # writes close values for the respective Date
 driver.quit()
